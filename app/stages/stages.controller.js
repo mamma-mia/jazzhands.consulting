f2d2628b9567f52b1599214bcc5da9948266cf45
CJ R <hello@cjr.co.de>
2017-01-26 20:09:20 -0700

    :sparkles: Add gifs

--- app/stages/stages.controller.js
+++ app/stages/stages.controller.js
@@ -12,11 +12,11 @@
 			vm.stages = [{
 				title: 'Stage 1: Recruitment',
 				description: 'Volunteers will be recruited from around the world to be trained in the Jazz Hands Forever™ methodology.',
-				image: 'https://s-media-cache-ak0.pinimg.com/originals/40/43/8a/40438a691a06af2cda671fe287b68579.jpg'
+				image: 'https://slack-imgs.com/?c=1&url=http%3A%2F%2Fmedia3.giphy.com%2Fmedia%2F106QCYtKPDeIjm%2Fgiphy.gif'
 			}, {
 				title: 'Stage 2: Training',
 				description: 'Recruits will be put through a 2 week training program where they will master the Jazz Hands Forever™ methodology.',
-				image: 'https://tax.thomsonreuters.com/wp-content/images/cs-professional-suite/banners/training-and-consulting-banner.jpg'
+				image: 'https://media.giphy.com/media/jHwXMkk0VWVKU/giphy.gif'
 			}, {
 				title: 'Stage 3: Bussed to Rotation',
 				description: 'Upon graduating the training program, recruits are bussed to the border to begin their 4 week rotation.',
